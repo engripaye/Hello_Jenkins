4ea7fc4a0c531456de2ef0161fda7ade03e13499
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-04-12 03:02:16 +0100

    second jenkins commit

--- HelloJekins.java
+++ HelloJekins.java
@@ -1,5 +1,9 @@
-public public static void main(String[] args) {
+public class HelloJekins {
+
+public static void main(String[] args) {
     System.out.println(
         "Hello Jenkins! This is a test for Jenkins pipeline."
     );
+
+}
 }
